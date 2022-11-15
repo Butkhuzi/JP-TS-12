d5c0974018906ccbbbfb47ad1d3aff4699a1a8d6
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2022-11-15 18:23:06 +0400

    Interfaces added

---
+++ TodoApp/TodoApp.Services/Interfaces/IDataConnection.cs
@@ -0,0 +1,13 @@
+﻿using Todo.Library;
+
+namespace TodoApp.Services.Interfaces
+{
+    public interface IDataConnection
+    {
+        /// <summary>
+        /// Gets all users from database
+        /// </summary>
+        /// <returns>List of users</returns>
+        List<User> GetAllUsers();
+    }
+}
