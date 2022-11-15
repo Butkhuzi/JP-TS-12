ca6c45b327b4f9f93e1110e439b186fafc4caf29
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2022-11-15 18:23:19 +0400

    Interface implemented

---
+++ TodoApp/TodoApp.Services/SqlDataConnector.cs
@@ -0,0 +1,13 @@
+﻿using Todo.Library;
+using TodoApp.Services.Interfaces;
+
+namespace TodoApp.Services
+{
+    public class SqlDataConnector : IDataConnection
+    {
+        public List<User> GetAllUsers()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
