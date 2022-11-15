02de365185f556b19f517f73dc4a235a25395cc8
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2022-11-15 19:06:33 +0400

    Connection string added in global config

--- TodoApp/TodoApp.Services/GlobalConfig.cs
+++ TodoApp/TodoApp.Services/GlobalConfig.cs
@@ -2,5 +2,6 @@
 {
     public static class GlobalConfig
     {
+        public static string ConnectionString { get; } = @"Server=DESKTOP-ILO1R6I\SQLEXPRESS;Database=jpTs12Todo;Trusted_Connection=True;rustServerCertificate=True";
     }
 }
