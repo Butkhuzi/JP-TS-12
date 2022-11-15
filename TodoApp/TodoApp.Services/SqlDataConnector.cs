bd3871965577b0ad6937af8234e389ea38e6f747
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2022-11-15 19:06:46 +0400

    GetAllUsers method added

--- TodoApp/TodoApp.Services/SqlDataConnector.cs
+++ TodoApp/TodoApp.Services/SqlDataConnector.cs
@@ -1,5 +1,7 @@
-﻿using Todo.Library;
+﻿using Microsoft.Data.SqlClient;
+using Todo.Library;
 using TodoApp.Services.Interfaces;
+using System.Data;
 
 namespace TodoApp.Services
 {
@@ -7,7 +9,46 @@ namespace TodoApp.Services
     {
         public List<User> GetAllUsers()
         {
-            throw new NotImplementedException();
+            const string sqlExpression = "sp_allUsers";
+            List<User> result = new();
+
+            using (SqlConnection connection = new(GlobalConfig.ConnectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    SqlCommand command = new(sqlExpression, connection);
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        while (reader.Read())
+                        {
+                            result.Add(new User
+                            {
+                                UserId = reader.GetInt32(0),
+                                FirstName = reader.GetString(1),
+                                LastName = reader.GetString(2),
+                                FullName = reader.GetString(3),
+                                Email = reader.GetString(4)
+                            });
+                        }
+                    }
+                }
+                catch (SqlException)
+                {
+                    throw;
+                }
+                finally
+                {
+                    connection.Close();
+                }
+
+                return result;
+            }
+
         }
     }
 }
