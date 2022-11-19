fe984b6efde5e3049c61288b436d89e264e4d8ad
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2022-11-19 18:42:02 +0400

    GetAllTodosPerUser function implemented

--- TodoApp/TodoApp.Services/SqlDataConnector.cs
+++ TodoApp/TodoApp.Services/SqlDataConnector.cs
@@ -52,6 +52,61 @@ namespace TodoApp.Services
 
             return result;
         }
+        public List<TodoModel> GetAllTodosPerUser(UserModel model)
+        {
+            if (model is null)
+            {
+                throw new ArgumentNullException(nameof(model));
+            }
+
+            const string sqlExpression = "sp_allTodosPerUser";
+            List<TodoModel> result = new();
+
+            using (SqlConnection connection = new(GlobalConfig.ConnectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    SqlCommand command = new(sqlExpression, connection);
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    command.Parameters.AddWithValue("@userId", model.UserId);
+
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            result.Add(new TodoModel
+                            {
+                                TodoId = reader.GetInt32(0),
+                                Title = reader.GetString(1),
+                                Description = reader.GetString(2),
+                                StartDate = reader.GetDateTime(3),
+                                DueDate = reader.GetDateTime(4),
+                                Status = reader.GetString(5),
+                                Priority = reader.GetString(6),
+                                UserId = reader.GetInt32(7)
+                            });
+                        }
+                    }
+                }
+                catch (SqlException)
+                {
+                    throw;
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+
+
+
+            return result;
+        }
         public List<UserModel> GetAllUsers()
         {
             const string sqlExpression = "sp_allUsers";
