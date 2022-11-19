9666a336fb1bd1caaa84fd8175419e21be563450
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2022-11-19 19:14:01 +0400

    LoginUser test added

---
+++ TodoApp/TodoApp.Tests/LoginUserShould.cs
@@ -0,0 +1,33 @@
+﻿namespace TodoApp.Tests
+{
+    public class LoginUserShould
+    {
+        [Fact]
+        public void Return_Logged_In_User_From_Database()
+        {
+            SqlDataConnector sqlDataConnector = new();
+            UserModel argument = new()
+            {
+                UserId = 1,
+                FirstName = "ნიკა",
+                LastName = "ჩხარტიშვილი",
+                FullName = "ნიკა ჩხარტიშვილი",
+                Email = "nika.chkhartishvili@gmail.com"
+            };
+
+            var expected = sqlDataConnector.LoginUser(argument);
+            var actual = sqlDataConnector.LoginUser(argument);
+
+            Assert.Equal(expected, actual, new UserEquilityComparer());
+        }
+
+        [Fact]
+        public void Throw_Argument_Null_Exception_If_Argument_Is_Null()
+        {
+            SqlDataConnector sqlDataConnector = new();
+#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+            Assert.Throws<ArgumentNullException>(() => sqlDataConnector.LoginUser(null));
+#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
+        }
+    }
+}
