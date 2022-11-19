a0bb7831cf93033b0f42635cdcf01603ce30dff0
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2022-11-19 19:13:27 +0400

    LoginUser and RegisterUser methods added

--- TodoApp/TodoApp.Services/Interfaces/IDataConnection.cs
+++ TodoApp/TodoApp.Services/Interfaces/IDataConnection.cs
@@ -20,5 +20,17 @@ namespace TodoApp.Services.Interfaces
         /// <param name="model">User model</param>
         /// <returns>List of todos</returns>
         List<TodoModel> GetAllTodosPerUser(UserModel model);
+        /// <summary>
+        /// Logs in user in system.
+        /// </summary>
+        /// <param name="user">User that tries to log in</param>
+        /// <returns>Logged in user</returns>
+        UserModel LoginUser(UserModel user);
+        /// <summary>
+        /// Register user in database
+        /// </summary>
+        /// <param name="model">New user</param>
+        /// <returns>Registered user</returns>
+        UserModel RegisterUser(UserModel model);
     }
 }
