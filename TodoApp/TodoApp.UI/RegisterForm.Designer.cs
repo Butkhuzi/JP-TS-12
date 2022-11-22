f3e58c9fe9496fc5e061548a45212f125f547955
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2022-11-22 18:57:55 +0400

    Register form added

---
+++ TodoApp/TodoApp.UI/RegisterForm.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace TodoApp.UI
+{
+    partial class RegisterForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.headerLabel = new System.Windows.Forms.Label();
+            this.emailLabel = new System.Windows.Forms.Label();
+            this.emailValue = new System.Windows.Forms.TextBox();
+            this.lastNameValue = new System.Windows.Forms.TextBox();
+            this.lastNameLabel = new System.Windows.Forms.Label();
+            this.firstNameValue = new System.Windows.Forms.TextBox();
+            this.firstNameLabel = new System.Windows.Forms.Label();
+            this.registerBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // headerLabel
+            // 
+            this.headerLabel.AutoSize = true;
+            this.headerLabel.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.headerLabel.ForeColor = System.Drawing.Color.Coral;
+            this.headerLabel.Location = new System.Drawing.Point(92, 9);
+            this.headerLabel.Name = "headerLabel";
+            this.headerLabel.Size = new System.Drawing.Size(167, 31);
+            this.headerLabel.TabIndex = 3;
+            this.headerLabel.Text = "რეგისტრაცია";
+            // 
+            // emailLabel
+            // 
+            this.emailLabel.AutoSize = true;
+            this.emailLabel.ForeColor = System.Drawing.Color.Black;
+            this.emailLabel.Location = new System.Drawing.Point(12, 207);
+            this.emailLabel.Name = "emailLabel";
+            this.emailLabel.Size = new System.Drawing.Size(89, 20);
+            this.emailLabel.TabIndex = 5;
+            this.emailLabel.Text = "ელ-ფოსტა";
+            // 
+            // emailValue
+            // 
+            this.emailValue.Location = new System.Drawing.Point(12, 230);
+            this.emailValue.Name = "emailValue";
+            this.emailValue.Size = new System.Drawing.Size(328, 27);
+            this.emailValue.TabIndex = 3;
+            // 
+            // lastNameValue
+            // 
+            this.lastNameValue.Location = new System.Drawing.Point(12, 164);
+            this.lastNameValue.Name = "lastNameValue";
+            this.lastNameValue.Size = new System.Drawing.Size(328, 27);
+            this.lastNameValue.TabIndex = 2;
+            // 
+            // lastNameLabel
+            // 
+            this.lastNameLabel.AutoSize = true;
+            this.lastNameLabel.ForeColor = System.Drawing.Color.Black;
+            this.lastNameLabel.Location = new System.Drawing.Point(12, 141);
+            this.lastNameLabel.Name = "lastNameLabel";
+            this.lastNameLabel.Size = new System.Drawing.Size(89, 20);
+            this.lastNameLabel.TabIndex = 5;
+            this.lastNameLabel.Text = "ელ-ფოსტა";
+            // 
+            // firstNameValue
+            // 
+            this.firstNameValue.Location = new System.Drawing.Point(12, 92);
+            this.firstNameValue.Name = "firstNameValue";
+            this.firstNameValue.Size = new System.Drawing.Size(328, 27);
+            this.firstNameValue.TabIndex = 1;
+            // 
+            // firstNameLabel
+            // 
+            this.firstNameLabel.AutoSize = true;
+            this.firstNameLabel.ForeColor = System.Drawing.Color.Black;
+            this.firstNameLabel.Location = new System.Drawing.Point(12, 69);
+            this.firstNameLabel.Name = "firstNameLabel";
+            this.firstNameLabel.Size = new System.Drawing.Size(66, 20);
+            this.firstNameLabel.TabIndex = 5;
+            this.firstNameLabel.Text = "სახელი";
+            // 
+            // registerBtn
+            // 
+            this.registerBtn.BackColor = System.Drawing.Color.Coral;
+            this.registerBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.registerBtn.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.registerBtn.FlatAppearance.BorderSize = 0;
+            this.registerBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Sienna;
+            this.registerBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.registerBtn.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.registerBtn.ForeColor = System.Drawing.Color.White;
+            this.registerBtn.Location = new System.Drawing.Point(0, 308);
+            this.registerBtn.Name = "registerBtn";
+            this.registerBtn.Size = new System.Drawing.Size(348, 56);
+            this.registerBtn.TabIndex = 4;
+            this.registerBtn.Text = "რეგისტრაცია";
+            this.registerBtn.UseVisualStyleBackColor = false;
+            this.registerBtn.Click += new System.EventHandler(this.registerBtn_Click);
+            // 
+            // RegisterForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(348, 364);
+            this.Controls.Add(this.registerBtn);
+            this.Controls.Add(this.firstNameLabel);
+            this.Controls.Add(this.firstNameValue);
+            this.Controls.Add(this.lastNameLabel);
+            this.Controls.Add(this.lastNameValue);
+            this.Controls.Add(this.emailLabel);
+            this.Controls.Add(this.emailValue);
+            this.Controls.Add(this.headerLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "RegisterForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "რეგისტრაცია";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label headerLabel;
+        private Label emailLabel;
+        private TextBox emailValue;
+        private TextBox lastNameValue;
+        private Label lastNameLabel;
+        private TextBox firstNameValue;
+        private Label firstNameLabel;
+        private Button registerBtn;
+    }
+}
---
+++ TodoApp/TodoApp.UI/RegisterForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TodoApp.Services;
+
+namespace TodoApp.UI
+{
+    public partial class RegisterForm : Form
+    {
+        public RegisterForm()
+        {
+            InitializeComponent();
+        }
+
+        private void registerBtn_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+            //TODO -- Register logic
+        }
+    }
+}
