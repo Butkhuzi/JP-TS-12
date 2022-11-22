bd4b6ad60b83b028d261406ae67bd4ac4f79264d
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2022-11-22 18:58:33 +0400

    Database choice logic added in GlobalConfig

--- TodoApp/TodoApp.Services/GlobalConfig.cs
+++ TodoApp/TodoApp.Services/GlobalConfig.cs
@@ -1,7 +1,23 @@
-﻿namespace TodoApp.Services
+﻿using TodoApp.Library;
+using TodoApp.Services.Interfaces;
+
+namespace TodoApp.Services
 {
     public static class GlobalConfig
     {
         public static string ConnectionString { get; private set; } = @"Server=DESKTOP-ILO1R6I\SQLEXPRESS;Database=jpTs12Todo;Trusted_Connection=True;TrustServerCertificate=True";
+
+        public static IDataConnection? DataConnection { get; private set; }
+        public static IDataConnection? ChooseDatabase(DatabaseType type)
+        {
+            switch (type)
+            {
+                case DatabaseType.Sql:
+                    DataConnection = new SqlDataConnector();
+                    break;
+            }
+
+            return DataConnection;
+        }
     }
 }
